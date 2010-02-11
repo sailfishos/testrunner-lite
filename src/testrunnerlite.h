3c6be90ada06cfafbbb881cbd86113f7ea2dd9c2
Sampo Saaristo <sampo@NULL.home.network>
2010-02-11 13:32:27 +0200

    autoconf and automake files

---
+++ src/testrunnerlite.h
@@ -0,0 +1,51 @@
+/* * This file is part of testrunnerlite *
+ *
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * All rights reserved.
+ *
+ * Contact: Sampo Saaristo <ext-sampo.1.saaristo@nokia.com>
+ *
+ * This software, including documentation, is protected by copyright
+ * controlled by Nokia Corporation. All rights are reserved. Copying,
+ * including reproducing, storing, adapting or translating, any or all of
+ * this material requires the prior written consent of Nokia Corporation.
+ * This material also contains confidential information which may not be
+ * disclosed to others without the prior written consent of Nokia.
+ *
+ */
+
+#ifndef TESTRUNNERLITE_H
+#define TESTRUNNERLITE_H
+
+/* ------------------------------------------------------------------------- */
+/* INCLUDES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* CONSTANTS */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* MACROS */
+#define LOCAL static
+#define PROGNAME "testrunner-lite"
+/* ------------------------------------------------------------------------- */
+/* DATA TYPES */
+/* ------------------------------------------------------------------------- */
+/* None */
+    
+/* ------------------------------------------------------------------------- */
+/* FORWARD DECLARATIONS */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* STRUCTURES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* FUNCTION PROTOTYPES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+#endif                          /* TESTRUNNERLITE_H */
+/* End of file */
