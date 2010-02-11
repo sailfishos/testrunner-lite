14c287db8b5bf99852e200b4c1e79840c18b6346
Sampo Saaristo <sampo@NULL.home.network>
2010-02-11 13:22:50 +0200

    Work started. Just basic commandline options parsing.

---
+++ src/main.c
@@ -0,0 +1,204 @@
+/* * This file is part of testrunnerlite *
+ *
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * All rights reserved.
+ *
+ * Contact: Sampo Saaristo <ext-sampo.1.saaristo@nokia.com>
+ *
+ * This software, including documentation, is protected by copyright
+ * controlled by Nokia Corporation. All rights are reserved. Copying,
+ * including reproducing, storing, adapting or translating, any or all of
+ * this material requires the prior written consent of Nokia Corporation.
+ * This material also contains confidential information which may not be
+ * disclosed to others without the prior written consent of Nokia.
+ *
+ */
+
+/* ------------------------------------------------------------------------- */
+/* INCLUDE FILES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <errno.h>
+#include <string.h>
+
+#include "testrunnerlite.h"
+/* ------------------------------------------------------------------------- */
+/* EXTERNAL DATA STRUCTURES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* EXTERNAL GLOBAL VARIABLES */
+extern char* optarg;
+
+/* ------------------------------------------------------------------------- */
+/* EXTERNAL FUNCTION PROTOTYPES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* GLOBAL VARIABLES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* CONSTANTS */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* MACROS */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* LOCAL GLOBAL VARIABLES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* LOCAL CONSTANTS AND MACROS */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* MODULE DATA STRUCTURES */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* LOCAL FUNCTION PROTOTYPES */
+/* ------------------------------------------------------------------------- */
+LOCAL void usage();
+/* ------------------------------------------------------------------------- */
+
+/* ------------------------------------------------------------------------- */
+/* FORWARD DECLARATIONS */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* ==================== LOCAL FUNCTIONS ==================================== */
+/* ------------------------------------------------------------------------- */
+/** Print short help.
+ */
+LOCAL void usage()
+{
+	printf ("\nUsage: testrunnerlite [options]\n");
+	printf ("\Example: testrunnerlite -f tests.xml -o ~/results.xml "
+		"-e hardware\n");
+	printf ("\nOptions:\n");
+	printf ("  -h, --help\tShow this help message and exit.\n");
+	printf ("  -f FILE, --file=FILE\tInput file with test definitions "
+		"in XML (required).\n");
+	printf ("  -o FILE, --output=FILE\n\t\t"
+		"Output file for test results (required).\n");
+	printf ("  -r FORMAT, --format=FORMAT\n\t\t"
+		"Output file format. FORMAT can be xml or text.\n\t\t"
+		"Default: xml\n");
+	printf ("  -e ENVIRONMENT, --environment=ENVIRONMENT\n\t\t"
+		"Target test environment. Default: hardware\n");
+	printf ("  -v, --verbose\tEnable verbosity mode to show "
+		"debug messages.\n");
+	printf ("  -a, --automatic\tEnable only automatic tests "
+		"to be executed.\n");
+	printf ("  -m, --manual\tEnable only manual tests to be executed.\n");
+	
+	printf ("  -l FILTER, --filter=FILTER\n\t\t"
+		"Filtering option to select tests (not) to be executed.\n\t\t"
+		"E.g. '-testcase=bad_test -type=unknown' first disables\n\t\t"
+		"test case named as bad_test. Next, all tests with type\n\t\t"
+		"unknown are disabled. The remaining tests will be\n\t\t"
+		"executed.\n");
+	printf ("  -c, --ci\tDisable validation of test "
+		"definition against schema.\n");
+	return;
+}
+
+/* ------------------------------------------------------------------------- */
+/* ======================== FUNCTIONS ====================================== */
+/* ------------------------------------------------------------------------- */
+/** main() for testrunnerlite - handle command line switches and call parser
+ *  @param argc argument count
+ *  @param argv arguments
+ *  @param evnp environment
+ *  @return EXIT_SUCCESS if all is well
+ */
+int main (int argc, char *argv[], char *envp[])
+{
+	int h_flag = 0, v_flag = 0, a_flag = 0, m_flag = 0, c_flag = 0;
+	int opt_char, option_idx;
+	FILE *ifile = NULL, *ofile = NULL;
+	int retval = EXIT_SUCCESS;
+	
+	struct option testrunnerlite_options[] =
+		{
+			{"help", no_argument, &h_flag, 1},
+			{"file", required_argument, NULL, 'f'},
+			{"output", required_argument, NULL, 'o'},
+			{"format", required_argument, NULL, 'r'},
+			{"environment", required_argument, NULL, 'e'},
+			{"verbose", no_argument, &v_flag, 1},
+			{"automatic", no_argument, &a_flag, 1},
+			{"manual", no_argument, &m_flag, 1},
+			{"filter", required_argument, NULL, 'l'},
+			{"ci", no_argument, &c_flag}
+		};
+
+	while (1) {
+		option_idx = 0;
+     
+		opt_char = getopt_long (argc, argv, "hvamcf:o:e:l:",
+					testrunnerlite_options, &option_idx);
+		if (opt_char == -1)
+			break;
+		
+		switch (opt_char)
+		{
+		case 'h':
+			h_flag = 1;
+			break;
+		case 'v':
+			v_flag = 1;
+			break;
+		case 'a':
+			a_flag = 1;
+			break;
+		case 'm':
+			m_flag = 1;
+			break;
+		case 'c':
+			c_flag = 1;
+			break;
+		case 'f':
+			ifile = fopen (optarg, "r");
+			if (!ifile) {
+				fprintf (stderr, "%s Failed to open %s %s\n",
+					 PROGNAME, optarg, strerror (errno));
+				retval = EXIT_FAILURE;
+				goto OUT;
+			}
+			break;
+		case 'o':
+			ofile = fopen (optarg, "w");
+			if (!ofile) {
+				fprintf (stderr, "%s Failed to open %s %s\n",
+					 PROGNAME, optarg, strerror (errno));
+				retval = EXIT_FAILURE;
+				goto OUT;
+			}
+			break;
+		}
+	}
+	
+	if (h_flag)
+	  usage();
+	
+	
+OUT:
+	if (ifile)
+	  fclose (ifile);
+	if (ofile)
+	  fclose (ofile);
+
+	return retval;
+}	
+
+
+/* ================= OTHER EXPORTED FUNCTIONS ============================== */
+/* None */
+
+/* ------------------------------------------------------------------------- */
+/* End of file */
