15596fd8fbee23e08574d13e3e8ed42bcf599b5a
Sampo Saaristo <ssaaristo@DTSSSA.(none)>
2010-04-07 13:45:49 +0300

    tests for remote execution. Note! run only if id_rsa.pub found.

--- tests/ut_testexecutor.c
+++ tests/ut_testexecutor.c
@@ -130,7 +130,8 @@ START_TEST (test_executor_long_input_streams)
 	init_exec_data(&edata);
 	edata.soft_timeout = 2;
 	edata.hard_timeout = 1;
-	fail_if (execute("/usr/share/testrunner-lite-tests/long_output.sh", &edata));
+	fail_if (execute("/usr/share/testrunner-lite-tests/long_output.sh", 
+			 &edata));
 	fail_unless (edata.result == 0);
 	fail_if (edata.stdout_data.buffer == NULL);
 	fail_if (edata.stderr_data.buffer == NULL);
@@ -151,10 +152,13 @@ START_TEST (test_executor_terminating_process)
 	fail_unless (edata.result == SIGTERM);
 	fail_if (edata.stdout_data.buffer == NULL);
 	fail_if (edata.stderr_data.buffer == NULL);
-	fail_unless (strcmp((char*)edata.stdout_data.buffer, "stdouttest") == 0);
-	fail_unless (strcmp((char*)edata.stderr_data.buffer, "stderrtest") == 0);
+	fail_unless (strcmp((char*)edata.stdout_data.buffer, 
+			    "stdouttest") == 0);
+	fail_unless (strcmp((char*)edata.stderr_data.buffer, 
+			    "stderrtest") == 0);
 	fail_if(edata.failure_info.buffer == NULL);
-	fail_unless (strcmp((char*)edata.failure_info.buffer, FAILURE_INFO_TIMEOUT) == 0);
+	fail_unless (strcmp((char*)edata.failure_info.buffer, 
+			    FAILURE_INFO_TIMEOUT) == 0);
 END_TEST
 /* ------------------------------------------------------------------------- */
 START_TEST (test_executor_killing_process)
@@ -168,10 +172,13 @@ START_TEST (test_executor_killing_process)
 	fail_unless (edata.result == SIGTERM || edata.result == SIGKILL);
 	fail_if (edata.stdout_data.buffer == NULL);
 	fail_if (edata.stderr_data.buffer == NULL);
-	fail_unless (strcmp((char*)edata.stdout_data.buffer, "stdouttest") == 0);
-	fail_unless (strcmp((char*)edata.stderr_data.buffer, "stderrtest") == 0);
+	fail_unless (strcmp((char*)edata.stdout_data.buffer, 
+			    "stdouttest") == 0);
+	fail_unless (strcmp((char*)edata.stderr_data.buffer, 
+			    "stderrtest") == 0);
 	fail_if(edata.failure_info.buffer == NULL);
-	fail_unless (strcmp((char*)edata.failure_info.buffer, FAILURE_INFO_TIMEOUT) == 0);
+	fail_unless (strcmp((char*)edata.failure_info.buffer, 
+			    FAILURE_INFO_TIMEOUT) == 0);
 END_TEST
 /* ------------------------------------------------------------------------- */
 START_TEST (test_executor_piped_command)
@@ -184,7 +191,8 @@ START_TEST (test_executor_piped_command)
 	fail_unless (edata.result == 0);
 	fail_if (edata.stdout_data.buffer == NULL);
 	fail_unless (edata.stdout_data.length == strlen("hello world\n"));
-	fail_unless (strcmp((char*)edata.stdout_data.buffer, "hello world\n")==0);
+	fail_unless (strcmp((char*)edata.stdout_data.buffer, 
+			    "hello world\n") == 0);
 END_TEST
 /* ------------------------------------------------------------------------- */
 START_TEST (test_executor_without_output_redirection)
@@ -222,7 +230,110 @@ START_TEST (test_executor_exec_data_handling)
 	fail_unless (edata.stderr_data.buffer == NULL);
 END_TEST
 /* ------------------------------------------------------------------------- */
+START_TEST (test_executor_remote_command)
+	exec_data edata;
+	testrunner_lite_options opts;
+	int ret;
 
+	opts.target_address = "localhost";
+	executor_init (&opts);
+	init_exec_data (&edata);
+	
+
+	ret = system ("grep -f ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys");
+	if (ret)
+		ret = system ("cat ~/.ssh/id_rsa.pub "
+			      ">> ~/.ssh/authorized_keys");
+	fail_if (ret, "failed to patch authorized_keys");
+
+	fail_if (execute("echo testing", &edata));
+	fail_unless (edata.result == 0);
+	fail_unless (edata.stdout_data.length == strlen("testing\n"));
+	fail_unless (strcmp((char*)edata.stdout_data.buffer, "testing\n") == 0);
+
+	clean_exec_data(&edata);
+	fail_unless (edata.stdout_data.buffer == NULL);
+	fail_unless (edata.stderr_data.buffer == NULL);
+
+	init_exec_data(&edata);
+	fail_if (execute("cat unexisting_foobar_file", &edata));
+	fail_if (edata.result == 0);
+	fail_if (edata.stderr_data.length == 0);
+	fail_unless (strlen((char*)edata.stderr_data.buffer) > 0);
+
+	clean_exec_data(&edata);
+	fail_unless (edata.stdout_data.buffer == NULL);
+	fail_unless (edata.stderr_data.buffer == NULL);
+END_TEST
+/* ------------------------------------------------------------------------- */
+START_TEST (test_executor_remote_non_ex_host)
+	exec_data edata;
+	testrunner_lite_options opts;
+
+	opts.target_address = "ww.example.com";
+	executor_init (&opts);
+	init_exec_data (&edata);
+	
+	fail_if (execute("echo testing", &edata));
+	fail_if (edata.result == 0);
+
+END_TEST
+/* ------------------------------------------------------------------------- */
+START_TEST (test_executor_remote_terminating_process)
+	exec_data edata;
+	testrunner_lite_options opts;
+
+	opts.target_address = "localhost";
+	executor_init (&opts);
+	
+	init_exec_data(&edata);
+	edata.soft_timeout = 3;
+	edata.hard_timeout = 3;
+	fail_if (execute("/usr/lib/testrunner-lite-tests/terminating " 
+			 "stdouttest stderrtest", &edata));
+	fail_unless (edata.result == SIGTERM);
+	fail_if (edata.stdout_data.buffer == NULL);
+	fail_if (edata.stderr_data.buffer == NULL);
+	fail_unless (strcmp((char*)edata.stdout_data.buffer, 
+			    "stdouttest") == 0);
+	fail_unless (strcmp((char*)edata.stderr_data.buffer, 
+			    "stderrtest") == 0);
+	fail_if(edata.failure_info.buffer == NULL);
+	fail_unless (strcmp((char*)edata.failure_info.buffer, 
+			    FAILURE_INFO_TIMEOUT) == 0);
+	/* check that killing was succesfull */
+	fail_if (execute("pidof terminating", &edata));
+	fail_unless (edata.result == 1);
+
+END_TEST
+/* ------------------------------------------------------------------------- */
+START_TEST (test_executor_remote_killing_process)
+	exec_data edata;
+	testrunner_lite_options opts;
+
+	opts.target_address = "localhost";
+	executor_init (&opts);
+
+	init_exec_data(&edata);
+	edata.soft_timeout = 1;
+	edata.hard_timeout = 1;
+	fail_if (execute("/usr/lib/testrunner-lite-tests/unterminating "
+			 "stdouttest stderrtest", &edata));
+	fail_unless (edata.result == SIGTERM || edata.result == SIGKILL);
+	fail_if (edata.stdout_data.buffer == NULL);
+	fail_if (edata.stderr_data.buffer == NULL);
+	fail_unless (strcmp((char*)edata.stdout_data.buffer, 
+			    "stdouttest") == 0);
+	fail_unless (strcmp((char*)edata.stderr_data.buffer, 
+			    "stderrtest") == 0);
+	fail_if(edata.failure_info.buffer == NULL);
+	fail_unless (strcmp((char*)edata.failure_info.buffer, 
+			    FAILURE_INFO_TIMEOUT) == 0);
+
+	fail_if (execute("pidof unterminating", &edata));
+	fail_unless (edata.result == 1);
+
+END_TEST
 /* ------------------------------------------------------------------------- */
 /* ======================== FUNCTIONS ====================================== */
 /* ------------------------------------------------------------------------- */
@@ -233,6 +344,7 @@ Suite *make_testexecutor_suite (void)
 
     /* Create test cases and add to suite. */
     TCase *tc;
+    int ret;
 
     tc = tcase_create ("Test executor with null command.");
     tcase_add_test (tc, test_executor_null_command);
@@ -276,6 +388,25 @@ Suite *make_testexecutor_suite (void)
     tc = tcase_create ("Test executor execution data handling.");
     tcase_add_test (tc, test_executor_exec_data_handling);
     suite_add_tcase (s, tc);
+
+    tc = tcase_create ("Test executor remote command.");
+    tcase_add_test (tc, test_executor_remote_command);
+    suite_add_tcase (s, tc);
+
+    tc = tcase_create ("Test executor remote non-existent host.");
+    tcase_add_test (tc, test_executor_remote_non_ex_host);
+    suite_add_tcase (s, tc);
+
+    tc = tcase_create ("Test executor remote terminating process.");
+    tcase_set_timeout (tc, 5);
+    tcase_add_test (tc, test_executor_remote_terminating_process);
+    suite_add_tcase (s, tc);
+
+    tc = tcase_create ("Test executor remote killing process.");
+    tcase_set_timeout (tc, 5);
+    tcase_add_test (tc, test_executor_remote_killing_process);
+    suite_add_tcase (s, tc);
+
 
     return s;
 }
